adf9101944268fd2265c448150b9101406d90a46
delanobgt <irvindelano@gmail.com>
2018-10-12 00:09:42 +0700

    if_exercise_1

---
+++ 5_if.py
@@ -0,0 +1,26 @@
+
+# if statement
+
+# single
+if 3 < 5:
+    print ('3 memang kurang dari 5')
+
+# else
+if 3 > 5:
+    print ('3 lebih dari 5')
+else:
+    print ('3 tidak lebih dari 5')
+
+# elif
+if 3 > 5:
+    print ('3 lebih dari 5')
+elif 6 < 7:
+    print ('6 kurang dari 7')
+
+# elif, else
+if 3 > 5:
+    print ('3 lebih dari 5')
+elif 6 > 7:
+    print ('6 lebih dari 7')
+else:
+    print ('semua kondisi salah')
