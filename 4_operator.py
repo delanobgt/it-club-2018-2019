3b043029984a2fd3a0001f47bd97c29e734b8894
delanobgt <irvindelano@gmail.com>
2018-10-11 22:51:41 +0700

    operators

---
+++ 4_operator.py
@@ -0,0 +1,20 @@
+
+# arithmetic operators
+
+print (1+1)
+print (3-2)
+print (3*3)
+print (9/2) # normal division
+print (9//2) # floor division
+print (2**4) # exponent
+
+# logical operators
+
+print (1 == 1)
+print (2 != 3)
+print (2 <= 3)
+print (4 >= 3)
+print (1 < 3)
+print (5 > 3)
+
+
