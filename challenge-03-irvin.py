6bfb685bf6c191fbd1e63f07b4913154c9347298
delanobgt <irvindelano@gmail.com>
2018-10-19 13:27:45 +0700

    zodiac solution fix

--- challenge-03-irvin.py
+++ challenge-03-irvin.py
@@ -2,11 +2,16 @@ month_names = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', '
 pivots = [21, 19, 21, 21, 21, 22, 23, 24, 24, 24, 23, 22, 21, 19]
 zodiac_names = ['Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn']
 
-print('Zodiak Determinator')
-day = int(input('Input tanggal: '))
-month = int(input('Input bulan: ')) - 1
+repeat = 'y'
+while repeat == 'y':
+    print('Zodiak Determinator')
+    day = int(input('Input tanggal: '))
+    month = int(input('Input bulan: ')) - 1
 
-if day < pivots[month]:
-    print(f'Zodiak pada tanggal {day} {month_names[month]} adalah : {zodiac_names[(month-1+12)%12]}')
-else:
-    print(f'Zodiak pada tanggal {day} {month_names[month]} adalah : {zodiac_names[month]}')
+    if day < pivots[month]:
+        print(f'Zodiak pada tanggal {day} {month_names[month]} adalah : {zodiac_names[(month-1+12)%12]}')
+    else:
+        print(f'Zodiak pada tanggal {day} {month_names[month]} adalah : {zodiac_names[month]}')
+
+    repeat = input('Continue?(y/n) : ')
+    print()
--- challenge-03-wesley.py
+++ challenge-03-wesley.py
@@ -6,29 +6,29 @@ while repeat == 'y':
     tgl = int(input('Input tanggal : '))
     bln = int(input('Input bulan : '))
  
-    if (20 <= tgl <= 31 and bln == 1) or (1 <= tgl <= 18 and bln == 2):
+    if (21 <= tgl <= 31 and bln == 1) or (1 <= tgl <= 18 and bln == 2):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Aquarius')
     elif (19 <= tgl <= 29 and bln == 2) or (1 <= tgl <= 20 and bln == 3):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Pisces')
-    elif (21 <= tgl <= 31 and bln == 3) or (1 <= tgl <= 19 and bln == 4):
+    elif (21 <= tgl <= 31 and bln == 3) or (1 <= tgl <= 20 and bln == 4):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Aries')
-    elif (20 <= tgl <= 30 and bln == 4) or (1 <= tgl <= 20 and bln == 5):
+    elif (21 <= tgl <= 30 and bln == 4) or (1 <= tgl <= 20 and bln == 5):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Taurus')
-    elif (21 <= tgl <= 31 and bln == 5) or (1 <= tgl <= 20 and bln == 6):
+    elif (21 <= tgl <= 31 and bln == 5) or (1 <= tgl <= 21 and bln == 6):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Gemini')
-    elif (21 <= tgl <= 30 and bln == 6) or (1 <= tgl <= 22 and bln == 7):
+    elif (22 <= tgl <= 30 and bln == 6) or (1 <= tgl <= 22 and bln == 7):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Cancer')
-    elif (23 <= tgl <= 31 and bln == 7) or (1 <= tgl <= 22 and bln == 8):
+    elif (23 <= tgl <= 31 and bln == 7) or (1 <= tgl <= 23 and bln == 8):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Leo')
-    elif (23 <= tgl <= 31 and bln == 8) or (1 <= tgl <= 22 and bln == 9):
+    elif (24 <= tgl <= 31 and bln == 8) or (1 <= tgl <= 23 and bln == 9):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Virgo')
-    elif (23 <= tgl <= 30 and bln == 9) or (1 <= tgl <= 22 and bln == 10):
+    elif (24 <= tgl <= 30 and bln == 9) or (1 <= tgl <= 23 and bln == 10):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Libra')
-    elif (23 <= tgl <= 31 and bln == 10) or (1 <= tgl <= 21 and bln == 11):
+    elif (24 <= tgl <= 31 and bln == 10) or (1 <= tgl <= 22 and bln == 11):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Scorpio')
-    elif (22 <= tgl <= 30 and bln == 11) or (1 <= tgl <= 21 and bln == 12):
+    elif (23 <= tgl <= 30 and bln == 11) or (1 <= tgl <= 21 and bln == 12):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Sagittarius')
-    elif (22 <= tgl <= 31 and bln == 12) or (1 <= tgl <= 19 and bln == 1):
+    elif (22 <= tgl <= 31 and bln == 12) or (1 <= tgl <= 20 and bln == 1):
         print('Zodiak pada tanggal '+ str(tgl)+' '+nm_bln[bln-1]+' adalah : Capricorn')
     else:
         print('Tanggal anda salah, coba lagi :)')
